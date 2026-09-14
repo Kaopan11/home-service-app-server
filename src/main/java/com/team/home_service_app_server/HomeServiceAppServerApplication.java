e2ebde608543c373368e2363e8430c75260b2402
Kaopan <atkaew@gmail.com>
2026-09-14 13:50:39 +0700

    Initial commit: Spring Boot server

---
+++ src/main/java/com/team/home_service_app_server/HomeServiceAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.team.home_service_app_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/home_service_app_server/HomeServiceAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.home_service_app_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServiceAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
