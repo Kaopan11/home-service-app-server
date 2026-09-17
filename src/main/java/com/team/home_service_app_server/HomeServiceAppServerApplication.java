d56968e45c790a4ae9f38b494299a77e2ff47cb0
Natchanon <nutchanoon.yen@gmail.com>
2026-09-17 12:18:04 +0700

    fix(env): auto-load .env on application startup

--- src/main/java/com/team/home_service_app_server/HomeServiceAppServerApplication.java
+++ src/main/java/com/team/home_service_app_server/HomeServiceAppServerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class HomeServiceAppServerApplication {
 
 	public static void main(String[] args) {
+		com.team.home_service_app_server.config.DotenvLoader.copyIntoSystemProperties(
+				com.team.home_service_app_server.config.DotenvLoader.read());
 		SpringApplication.run(HomeServiceAppServerApplication.class, args);
 	}
 
